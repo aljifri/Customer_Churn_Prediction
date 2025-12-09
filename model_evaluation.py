930c12d6289f7eccaa5ada564530c8cdeecdd868
aljifri <mohammed.gafri@rwth-aachen.de>
2025-12-09 03:38:10 +0300

    Fix confusion matrix and report argument order

--- model_evaluation.py
+++ model_evaluation.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import classification_report, confusion_matrix
 from model_training import predictions, y_test
 
-print(confusion_matrix(predictions, y_test))
-print(classification_report(predictions, y_test))
+print(confusion_matrix(y_test, predictions))
+
+print(classification_report(y_test, predictions))
